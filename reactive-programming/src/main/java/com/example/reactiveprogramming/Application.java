cda57904efdf2ea9f7dbe44fb0c1cca577a2ead1
Dhruv Maheshwari <mrdm026@gmail.com>
2024-05-29 12:39:46 +0530

    Created module for the movie service project

--- reactive-programming/src/main/java/com/example/reactiveprogramming/Application.java
+++ reactive-programming/src/main/java/com/example/reactiveprogramming/Application.java
@@ -1,6 +1,7 @@
-package com.example.springreactiveprogramming;
+package com.example.reactiveprogramming;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
